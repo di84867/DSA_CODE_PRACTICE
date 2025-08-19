b43987288fc399bc00e7f9e6117a374d1a58e42b
Divyansh Singh <singhdivyansh84867@gmail.com>
2025-08-19 12:41:04 +0530

    matrix

--- JAVA/DiagAntiDiagMatrix.java
+++ JAVA/DiagAntiDiagMatrix.java
@@ -1,16 +1,39 @@
 import java.util.Scanner;
 
 public class DiagAntiDiagMatrix {
+
+    static void printDiagonal(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        int r = 0, c = 0;
+        while (r < n && c < m) {
+            System.out.print(mat[r][c] + " ");
+            r++;
+            c++;
+        }
+        System.out.println();
+    }
+
+    static void printAntiDiagonal(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        int r = 0, c = m - 1;
+        while (r < n && c >= 0) {
+            System.out.print(mat[r][c] + " ");
+            r++;
+            c--;
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Input matrix size (square matrix)
         System.out.print("Enter size of square matrix: ");
         int n = scanner.nextInt();
 
         int[][] matrix = new int[n][n];
 
-        // Input matrix elements
         System.out.println("Enter the elements of the matrix:");
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -18,7 +41,7 @@ public class DiagAntiDiagMatrix {
             }
         }
 
-         // Print Original Matrix
+        // Print Original Matrix
         System.out.println("\nOriginal Matrix:");
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -26,32 +49,14 @@ public class DiagAntiDiagMatrix {
             }
             System.out.println();
         }
-        
-        // Print Diagonal Matrix
-        System.out.println("\nDiagonal Matrix:");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j) {
-                    System.out.print(matrix[i][j] + "\t");
-                } else {
-                    System.out.print("0\t");
-                }
-            }
-            System.out.println();
-        }
 
-        // Print Anti-Diagonal Matrix
-        System.out.println("\nAnti-Diagonal Matrix:");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i + j == n - 1) {
-                    System.out.print(matrix[i][j] + "\t");
-                } else {
-                    System.out.print("0\t");
-                }
-            }
-            System.out.println();
-        }
+        // Print main diagonal elements
+        System.out.println("\nMain Diagonal Elements:");
+        printDiagonal(matrix);
+
+        // Print anti-diagonal elements
+        System.out.println("Anti-Diagonal Elements:");
+        printAntiDiagonal(matrix);
 
         scanner.close();
     }
